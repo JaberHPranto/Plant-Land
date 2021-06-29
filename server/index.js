b3cc4cd0b89897a8d64fd020d0d0005db5fdd0f6
JaberHPranto <jhpranto53@gmail.com>
2021-06-30 00:10:47 +0600

    mongodb connection done

--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './config/db.js';
 
 const app = express()
 dotenv.config()
+
+
+// middlewares
+app.use(express.json({limit:'30mb',extended:true}))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
 
 
 app.get("/",(req,res) => {
     res.send("Hello world")
 })
 
-const PORT = process.env.PORT || 5000
+// connect to database
+connectDB()
 
+const PORT = process.env.PORT
 app.listen(PORT, (req, res) => {
     console.log(`Server started on port ${PORT}` );
 })
