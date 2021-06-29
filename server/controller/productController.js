8886963c0f649e5d977d99e74f8b909ebd9dec53
JaberHPranto <jhpranto53@gmail.com>
2021-06-30 03:54:02 +0600

    getting products from database

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,9 +1,16 @@
 import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
 
 export const getProducts = asyncHandler(async (req, res) => {
-    res.send("Getting Products...")
+    const products = await Product.find()
+        res.json(products)
 })
 
-export const getProductById = asyncHandler(async (req, res) => {
-    res.send("Getting Product by id...")
+export const getProductById = (async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id)
+        res.json({product}) 
+    } catch(err){
+        res.status(404).json({message:"No product found"})
+    }
 })
