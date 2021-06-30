8db1dff3d38c4c5bbfb8cce36c067ca1ff1b9ec7
JaberHPranto <jhpranto53@gmail.com>
2021-06-30 23:57:47 +0600

    getting products from backend

--- client/src/components/Ecommerce/Screen/HomeScreen.js
+++ client/src/components/Ecommerce/Screen/HomeScreen.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Col, Row } from 'react-bootstrap'
-import products from '../../../products'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProducts } from '../../../redux/actions/productActions'
 import Product from '../Product'
 
+
 function HomeScreen() {
+
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+    
+
+    useEffect(() => {
+        dispatch(fetchProducts())
+    }, [dispatch])
+
+
     return (
         <div>
             <h2>Latest Products</h2>
-            <Row>
+            {loading ? 'loading....' : error ? <h1>{error}</h1> : (
+                <Row>
                 {products.map(product => (
                     <Col key={product._id} xs={12} md={6} lg={4} xl={3}>
                         <Product product={product} />
                     </Col>
                 ))}
             </Row>
+            )}
+            
         </div>
     )
 }
--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,12 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
+import { productListReducer } from './redux/reducers/productReducers'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    productList: productListReducer
+    
+})
 const initialState = {}
 const middlewares = [thunk]
 
