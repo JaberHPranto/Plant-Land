f12bd85509923fb12863407f46a827f680b196c7
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-07-01 22:04:51 +0600

    alvi

--- server/index.js
+++ server/index.js
@@ -1,35 +1,34 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import connectDB from './config/db.js';
-import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
-import productRoutes from './routes/productRoutes.js';
+import cors from "cors";
+import dotenv from "dotenv";
 
-const app = express()
-dotenv.config()
+import express from "express";
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 
+const app = express();
+dotenv.config();
 
 // middlewares
-app.use(express.json({limit:'30mb',extended:true}))
-app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors())
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-
-app.get("/",(req,res) => {
-    res.send("Hello world")
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 //routes
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
 
 // error handler
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 // connect to database
-connectDB()
+connectDB();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, (req, res) => {
-    console.log(`Server started on port ${PORT}` );
-})
+  console.log(`Server started on port ${PORT}`);
+});
