62903bdc004dd3cd623594f1518d4b5ff557ab05
moshiurrafialvi <moshiurrafi@iut-dhaka.edu>
2021-07-08 15:08:47 +0600

    Shipping Screen and Address saved of User

--- client/src/pages/EcommercePage.js
+++ client/src/pages/EcommercePage.js
@@ -1,36 +1,42 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "../components/Ecommerce/Footer";
 import Header from "../components/Ecommerce/Header";
-import CartScreen from '../components/Ecommerce/Screen/CartScreen';
-import HomeScreen from '../components/Ecommerce/Screen/HomeScreen';
-import LoginScreen from '../components/Ecommerce/Screen/LoginScreen';
-import ProductScreen from '../components/Ecommerce/Screen/ProductScreen';
-import ProfileScreen from '../components/Ecommerce/Screen/ProfileScreen';
-import RegisterScreen from '../components/Ecommerce/Screen/RegisterScreen';
-import '../styles/ecommerce.css';
+import CartScreen from "../components/Ecommerce/Screen/CartScreen";
+import HomeScreen from "../components/Ecommerce/Screen/HomeScreen";
+import LoginScreen from "../components/Ecommerce/Screen/LoginScreen";
+import ProductScreen from "../components/Ecommerce/Screen/ProductScreen";
+import ProfileScreen from "../components/Ecommerce/Screen/ProfileScreen";
+import RegisterScreen from "../components/Ecommerce/Screen/RegisterScreen";
+import "../styles/ecommerce.css";
+import ShippingScreen from "../components/Ecommerce/Screen/ShippingScreen";
 
 function EcommercePage() {
-    return (
-        <Router>
-            <Header />
-            <main>
-                <Container>
-                    <Route path="/" exact component={HomeScreen} />
-                    <Route path="/search/:keyword" component={HomeScreen} exact/>
-                    <Route path="/search/:keyword/page/:pageNumber" component={HomeScreen} />
-                    <Route path="/page/:pageNumber" component={HomeScreen} />
-                    <Route path="/login" component={LoginScreen} />
-                    <Route path="/register" component={RegisterScreen} />
-                    <Route path="/profile" component={ProfileScreen} />
-                    <Route path="/product/:id" component={ProductScreen} />
-                    <Route path="/cart/:id?" component={CartScreen} />
-                </Container>
-            </main>
-            <Footer />
-        </Router>
-    )
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} exact />
+          <Route
+            path="/search/:keyword/page/:pageNumber"
+            component={HomeScreen}
+          />
+          <Route path="/page/:pageNumber" component={HomeScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
-export default EcommercePage
+export default EcommercePage;
--- client/src/redux/reducers/cartReducers.js
+++ client/src/redux/reducers/cartReducers.js
@@ -1,33 +1,47 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../../constants/cartConstants';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from "../../constants/cartConstants";
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
-    switch (action.type) {
-        case CART_ADD_ITEM:
-            const item = action.payload
+export const cartReducer = (
+  state = { cartItems: [], shippingAddress: {} },
+  action
+) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
 
-            const isItemExist = state.cartItems.find(p => p.productId === item.productId)
-            if (isItemExist) {
-                return {
-                    ...state,
-                    cartItems: state.cartItems.map(p => p.productId === item.productId ? item : p)
-                }
-                
-            } else {
-                return {
-                    ...state,
-                    cartItems: [...state.cartItems, item]
-                    
-                }
-            }
-        case CART_REMOVE_ITEM:
-            return {
-                ...state,
-                cartItems: state.cartItems.filter(p=>p.productId !== action.payload)
-            }
-            
-        default:
-            return state
-    }
-    
-}
+      const isItemExist = state.cartItems.find(
+        (p) => p.productId === item.productId
+      );
+      if (isItemExist) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((p) =>
+            p.productId === item.productId ? item : p
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, item],
+        };
+      }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (p) => p.productId !== action.payload
+        ),
+      };
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...state,
+        shippingAddress: action.payload,
+      };
 
+    default:
+      return state;
+  }
+};
