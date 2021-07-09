3ab77a595589ff7aff35965ed97adc27f93b986c
JaberHPranto <jhpranto53@gmail.com>
2021-07-09 13:56:12 +0600

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-//import LandingPage from "./pages/LandingPage";
-import EcommercePage from "./pages/EcommercePage";
+import LandingPage from "./pages/LandingPage";
+// import EcommercePage from "./pages/EcommercePage";
 import "./styles/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-      {/* <LandingPage /> */}
-      <EcommercePage />
+      <LandingPage />
+      {/* <EcommercePage /> */}
     </div>
   );
 }
