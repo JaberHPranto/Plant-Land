82ae577fe84e2d480ed991ed587019ab56ea8643
JaberHPranto <jhpranto53@gmail.com>
2021-09-05 01:37:08 +0600

    done with create blog page

---
+++ client/src/components/Blog/BlogCategory.js
@@ -0,0 +1,42 @@
+import React from "react";
+const { useState} = React;
+
+const data = [{ id: "plant", label: "Plant" },
+    { id: "garden", label: "Gardening" },
+    { id: "diseases", label: "Plant Diseases" },
+    { id: "green", label: "Green Living" },
+    { id: "tools", label: "Gardening Tools" },
+    { id: "others", label: "Others" },
+];
+
+const BlogCategory = () => {
+  const [isOpen, setOpen] = useState(false);
+  const [items, setItem] = useState(data);
+  const [selectedItem, setSelectedItem] = useState(null);
+  
+  const toggleDropdown = () => setOpen(!isOpen);
+  
+    const handleItemClick = (id) => {
+    console.log(id);
+    selectedItem === id ? setSelectedItem(null) : setSelectedItem(id);
+  }
+  
+  return (
+    <div className='dropdown'>
+      <div className='dropdown-header' onClick={toggleDropdown}>
+        {selectedItem ? items.find(item => item.id === selectedItem).label : "Select Blog Category"}
+        <i className={`fa fa-chevron-right icon ${isOpen && "open"}`}></i>
+      </div>
+      <div className={`dropdown-body ${isOpen && 'open'}`}>
+        {items.map(item => (
+          <div className="dropdown-item" key={item.id}  onClick={e => handleItemClick(e.target.id)} id={item.id}>
+            <span className={`dropdown-item-dot ${item.id === selectedItem && 'selected'}`}>• </span>
+            {item.label}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default BlogCategory
--- client/src/components/Blog/NewBlog.js
+++ client/src/components/Blog/NewBlog.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
+import BlogDropdown from './BlogCategory'
 
 function NewBlog() {
+    const history = useHistory()
+    function backToBlog() {
+        history.push("/blog")
+    }
     return (
-        <div>
-            This is a new blog page
+        <div className="write">
+            <img className="writeImage"
+                src="https://images.unsplash.com/photo-1620118218439-a23223a4d223?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                alt="blog hero"
+            />
+            <form className="writeForm">
+                <div className="writeFormGroup">
+                    <label htmlFor="blogImage"><i className="writeIcon fas fa-plus"></i></label>
+                    <input type="file" name="blogImage" id="blogImage" style={{display:'none'}} />
+                    <input type="text" name="title" id="title" placeholder="Title" autoFocus={true} className="writeInput writeTitle"/>
+                </div>
+                <div className="writeCategory">
+                    <BlogDropdown />
+                </div>
+                <div className="writeFormGroup">
+                    <input type="text" name="tags" id="tags" className="writeInput writeTags" placeholder="Add tags, separated by comma..."/>
+                </div>
+                <div className="writeFormGroup">
+                    <textarea name="desc" id="desc" cols="60" rows="10" placeholder="Write about plants..." className="writeInput writeText" ></textarea>
+                </div>
+                
+                <Button className="writeSubmit bg-col-primary">Publish</Button>
+            </form>
+            <Button className='blogBack' onClick={()=>history.push("/blog")} >Back</Button>
         </div>
     )
 }
