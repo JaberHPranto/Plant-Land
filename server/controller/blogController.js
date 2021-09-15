dbbee518ccbbcef5f72905626d4f9b64c2391a35
JaberHPranto <jhpranto53@gmail.com>
2021-09-15 23:50:26 +0600

    getting all posts

--- server/controller/blogController.js
+++ server/controller/blogController.js
@@ -1,7 +1,14 @@
 import BlogPost from '../models/blogModel.js';
 // Get all blog posts
-export const getBlogs = (req, res) => {
-    res.send("Everything works !!!")
+export const getBlogs = async (req, res) => {
+    try {
+        const blogs = await BlogPost.find()
+        res.status(200).json(blogs)
+    } catch (err) {
+        res.status(401).json({
+            error: err.message
+        })
+    }
 }
 
 // Create a blog 
